c30d50252cbe4f595f5ef8dfdb02786b2f04898b
Murali Sathenapally <Murali.Sathenapally@gmail.com>
2022-08-16 15:28:13 +0000

    changes

--- secretmanager/modules/src/main.py
+++ secretmanager/modules/src/main.py
@@ -1,4 +1,7 @@
 import base64
+import json
+import google.cloud.logging
+import logging
 
 def process_pubsub_event(event, context):
   message = base64.b64decode(event['data']).decode('utf-8')
@@ -7,5 +10,9 @@ def process_pubsub_event(event, context):
 
   secret_name = msg.get('name')
   roation_time = msg['rotation']['nextRotationTime']
-
-  print("Rotate Secret:" + secret_name + " Next Roation Time:" + roation_time)
+  
+  logging_client = google.cloud.logging.Client()
+  cloud_logger = logging_client.logger('secret-rotation-log')
+  
+  log_entry = ("Rotate Secret: " + secret_name + ", Next Roation Time: " + roation_time)
+  cloud_logger.log(log_entry, severity='INFO')
