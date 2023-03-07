692690baadad2338ee17f4a25ae5db09f75c1e6a
muralisathenapally <78802406+muralisathenapally@users.noreply.github.com>
2023-03-07 20:42:56 +0530

    Update main.tf

--- vpc_service_controls/modules/main.tf
+++ vpc_service_controls/modules/main.tf
@@ -1,15 +1,12 @@
-provider "google" {
-}
-
-module "org_policy" {
-  source      = "terraform-google-modules/vpc-service-controls/google"
-  parent_id   = var.org_id
-  policy_name = var.policy_name
+resource "google_access_context_manager_access_policy" "access-policy" {
+  parent = var.org_id
+  title  = "Scoped Access Policy"
+  scopes = ["folders/${var.folder_id}"]
 }
 
 module "regular_service_perimeter_1" {
   source         = "terraform-google-modules/vpc-service-controls/google//modules/regular_service_perimeter"
-  policy         = module.org_policy.policy_id
+  policy         = google_access_context_manager_access_policy.access-policy.id
   perimeter_name = var.perimeter_name
   description    = var.description
   resources      = var.resources
