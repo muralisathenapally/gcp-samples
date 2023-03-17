ea4383310d0397777bd3f64324ad64837fae1a55
muralisathenapally <78802406+muralisathenapally@users.noreply.github.com>
2023-03-17 11:00:43 +0530

    Update main.tf

--- vpc_service_controls/modules/main.tf
+++ vpc_service_controls/modules/main.tf
@@ -4,6 +4,15 @@ resource "google_access_context_manager_access_policy" "access-policy" {
   scopes = ["folders/${var.folder_id}"]
 }
 
+module "access_level" {
+  source      = "terraform-google-modules/vpc-service-controls/google//modules/access_level"
+  policy      = google_access_context_manager_access_policy.access-policy.id
+  name        = var.access_level_name
+  members = var.members
+  regions = var.regions
+  ip_subnetworks = var.ip_subnetworks
+}
+  
 module "regular_service_perimeter_1" {
   source         = "terraform-google-modules/vpc-service-controls/google//modules/regular_service_perimeter"
   policy         = google_access_context_manager_access_policy.access-policy.id
