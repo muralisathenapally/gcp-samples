2c636e69fffc3d1282a3489dcc8747d283bc9ca5
murali <murali>
2023-04-05 11:02:36 +0000

    service account catalog

---
+++ service_account_catalog/modules/service_account/variables.tf
@@ -0,0 +1,23 @@
+variable "account_id" {
+
+}
+
+variable "display_name" {
+
+}
+
+variable "description" {
+
+}
+
+variable "project_id" {
+  
+}
+
+variable "roles" {
+  
+}
+
+variable "allowed_roles" {
+  
+}
---
+++ service_account_catalog/variables.tf
@@ -0,0 +1,32 @@
+variable "account_id" {
+      type        = string
+  description = "The account id that is used to generate the service account email address"
+}
+
+variable "display_name" {
+  type        = string
+  description = "Display names of the created service accounts (defaults to 'Terraform-managed service account')"
+  default     = "Terraform-managed service account"
+}
+
+variable "description" {
+  type        = string
+  description = "Default description of the created service accounts (defaults to no description)"
+  default     = ""
+}
+
+variable "project_id" {
+  type        = string
+  description = "Project id where service account will be created."
+}
+
+variable "allowed_roles" {
+  type = list(string)
+  description = "These are the only allowed roles"
+  default = ["roles/editor","roles/viewer","roles/compute.admin"]
+}
+
+variable "roles" {
+  type = list(string)
+  description = "Roles to be attached to service account after creation. Any roles other than the allowed_roles are ignored"
+}
