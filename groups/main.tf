d0fa138a6c0a27aeb624653e84dfbd04d636aa4e
muralisathenapally <78802406+muralisathenapally@users.noreply.github.com>
2024-06-18 15:31:30 +0530

    Update main.tf

--- groups/main.tf
+++ groups/main.tf
@@ -2,7 +2,7 @@ resource "google_cloud_identity_group" "group" {
   group_key {
     id = "example_group@example.com"
   }
-  parent = "your domain name"
+  parent = "customers/example.com"
   display_name = "Example Group"
   description  = "This is an example group."
 }
