c1c7f413732d31ade64f4bce002522ba41ca1089
muralisathenapally <78802406+muralisathenapally@users.noreply.github.com>
2024-06-27 21:01:56 +0530

    Update googleworkspace_group.tf

--- groups/googleworkspace_group.tf
+++ groups/googleworkspace_group.tf
@@ -3,7 +3,8 @@ provider "googleworkspace" {
   oauth_scopes = [
     "https://www.googleapis.com/auth/admin.directory.group",
     "https://www.googleapis.com/auth/admin.directory.user",
-    "https://www.googleapis.com/auth/apps.groups.settings"
+    "https://www.googleapis.com/auth/apps.groups.settings",
+    "https://www.googleapis.com/auth/admin.directory.group.readonly"
   ]
 }
 
