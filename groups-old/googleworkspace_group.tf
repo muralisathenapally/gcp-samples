fbe78bb3cc7e113813b950a55bb52a6bdd8506ad
muralisathenapally <78802406+muralisathenapally@users.noreply.github.com>
2024-07-02 18:30:56 +0530

    Update googleworkspace_group.tf

--- groups-old/googleworkspace_group.tf
+++ groups-old/googleworkspace_group.tf
@@ -3,7 +3,9 @@ provider "googleworkspace" {
   oauth_scopes = [
     "https://www.googleapis.com/auth/admin.directory.group",
     "https://www.googleapis.com/auth/admin.directory.user",
+    "https://www.googleapis.com/auth/admin.directory.userschema",
     "https://www.googleapis.com/auth/apps.groups.settings",
+    "https://www.googleapis.com/auth/iam"
   ]
 }
 
