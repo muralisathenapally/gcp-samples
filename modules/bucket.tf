ccef4f5b817f09227c1799f748ff8fb76ed0b5ce
muralisathenapally <78802406+muralisathenapally@users.noreply.github.com>
2024-07-16 20:30:55 +0530

    Update bucket.tf

--- modules/bucket.tf
+++ modules/bucket.tf
@@ -1,5 +1,9 @@
+provider "google" {
+  credentials      = "key.json"
+}
 resource "google_storage_bucket" "auto-expire" {
   name          = "ms-no-public-access-bucket"
   location      = "US"
   force_destroy = true
+  project = "psyched-circuit-405312"
 }
