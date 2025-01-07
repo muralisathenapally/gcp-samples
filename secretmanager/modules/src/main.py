76480e104edd5bd56d880a9b27c7ae38f22ca2df
Murali <murali@gmail.com>
2025-01-07 08:02:00 +0000

    update

--- secretmanager/modules/src/main.py
+++ secretmanager/modules/src/main.py
@@ -1,6 +1,5 @@
 import base64
 import json
-import google.cloud.logging
 import logging
 import os
 
@@ -11,36 +10,32 @@ from utils import delete_old_secret_versions
 project = os.environ.get('PROJECT')
 
 def process_pubsub_event(event, context):
-  message = base64.b64decode(event['data']).decode('utf-8')
-  msg = json.loads(message)
-  secret_name = msg.get('name')
+    message = base64.b64decode(event['data']).decode('utf-8')
+    msg = json.loads(message)
+    secret_name = msg.get('name')
 
-  if '-json-key' in secret_name:
-      #get sa name
-      sn = secret_name.rsplit('/', 1)[-1]
-      service_account_name = str(sn[:-9])
-      service_account_client = ServiceAccountsClient()
+    if "rotation" in msg:
+        if '-json-key' in secret_name:
+            #get sa name
+            sn = secret_name.rsplit('/', 1)[-1]
+            service_account_name = str(sn[:-9])
+            service_account_client = ServiceAccountsClient()
 
-      #construct sa email
-      service_account = f"{service_account_name}@{project}.iam.gserviceaccount.com"
-      sa_name = f"projects/{project}/serviceAccounts/{service_account}"
+            #construct sa email
+            service_account = f"{service_account_name}@{project}.iam.gserviceaccount.com"
+            sa_name = f"projects/{project}/serviceAccounts/{service_account}"
 
-      #delete delete old json key
-      service_account_client.delete_key(sa_name)
+            #delete delete old json key
+            service_account_client.delete_key(sa_name)
 
-      #add new json key
-      private_key = service_account_client.create_key(sa_name)
+            #add new json key
+            private_key = service_account_client.create_key(sa_name)
 
-      #delete old secret version
-      delete_old_secret_versions(project, sn)
+            #delete old secret version
+            delete_old_secret_versions(project, sn)
 
-      #add secret version
-      add_secret_version(project, sn, private_key)
+            #add secret version
+            add_secret_version(project, sn, private_key)
 
-  else:
-      roation_time = msg['rotation']['nextRotationTime']
-      logging_client = google.cloud.logging.Client()
-      cloud_logger = logging_client.logger('secret-rotation-log')
-
-      log_entry = ("Rotate Secret: " + secret_name + ", Next Roation Time: " + roation_time)
-      cloud_logger.log(log_entry, severity='INFO')
+            rotation_time = msg['rotation']['nextRotationTime']
+            print("Rotated Secret: " + sn + ", Next Rotation Time: " + rotation_time)
