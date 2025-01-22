693ddbb09cf3cbe1bd235e2f16d6858014579b59
Murali <murali@gmail.com>
2025-01-22 16:10:57 +0000

    changes

--- secretmanager/modules/src/main.py
+++ secretmanager/modules/src/main.py
@@ -2,6 +2,7 @@ import base64
 import json
 import logging
 import os
+import re
 
 from utils import ServiceAccountsClient
 from utils import add_secret_version
@@ -16,26 +17,37 @@ def process_pubsub_event(event, context):
 
     if "rotation" in msg:
         if '-json-key' in secret_name:
-            #get sa name
+            # Get service account name from secret name
             sn = secret_name.rsplit('/', 1)[-1]
-            service_account_name = str(sn[:-9])
+            service_account_name = sn[:-9]  # Strip "-json-key" suffix
             service_account_client = ServiceAccountsClient()
 
-            #construct sa email
-            service_account = f"{service_account_name}@{project}.iam.gserviceaccount.com"
+            # Determine the service account email domain
+            if service_account_name == project:
+                # If the service_account_name is equal to the project value
+                domain = "appspot.gserviceaccount.com"
+            elif re.match(r"^\d{13}-$", service_account_name):
+                # If the service_account_name is 13 digits followed by a hyphen
+                domain = "developer.gserviceaccount.com"
+            else:
+                # Default to the standard domain
+                domain = f"{project}.iam.gserviceaccount.com"
+
+            # Construct the service account email
+            service_account = f"{service_account_name}@{domain}"
             sa_name = f"projects/{project}/serviceAccounts/{service_account}"
 
-            #delete delete old json key
+            # Delete the old JSON key
             service_account_client.delete_key(sa_name)
 
-            #add new json key
+            # Add a new JSON key
             private_key = service_account_client.create_key(sa_name)
 
-            #delete old secret version
+            # Delete old secret versions
             delete_old_secret_versions(project, sn)
 
-            #add secret version
+            # Add a new secret version
             add_secret_version(project, sn, private_key)
 
             rotation_time = msg['rotation']['nextRotationTime']
-            print("Rotated Secret: " + sn + ", Next Rotation Time: " + rotation_time)
+            print(f"Rotated Secret: {sn}, Next Rotation Time: {rotation_time}")
